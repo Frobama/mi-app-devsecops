ef095cc98eb57323e752f5301191f0934e436447
Frobama <benjamin.rojas.espejo@gmail.com>
2026-05-28 22:41:17 -0400

    feat: inicializar proyecto Next.js con Typescript y Tailwind

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+module.exports = config;
